25a0b00e2160fde724a220204dbc59028724ac08
Juvance <148179019+Jivasa-toky@users.noreply.github.com>
2025-11-26 07:09:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-
-const http = require('node:http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+import { styleText } from 'node:util';
+console.log(
+  styleText(['red'], 'This is red text ') +
+    styleText(['green', 'bold'], 'and this is green bold text ') +
+    'this is normal text'
+);
